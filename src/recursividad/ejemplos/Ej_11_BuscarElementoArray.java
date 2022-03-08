2fbbd5f63f1f6b3da1e19b750bebd8347135748e
Jess <Jessjo.324@gmail.com>
2022-03-08 16:44:12 -0500

    Anexando ejercicio de recursividad

--- src/recursividad/ejemplos/Ej_11_BuscarElementoArray.java
+++ src/recursividad/ejemplos/Ej_11_BuscarElementoArray.java
@@ -4,15 +4,20 @@ public class Ej_11_BuscarElementoArray {
 
 	public static void main(String[] args) {
 
-		int[] array = {4, 78, 12, 3, 89, 0, 2};
-		int elem = 12;
+//		int[] array = {4, 78, 12, 3, 89, 0, 2};
+//		int elem = 12;
+//		
+//		int posElemEncontrado = buscarElementoArray(array, 0, elem);
 		
-		int posElemEncontrado = buscarElementoArray(array, 0, elem);
 		
-		if(posElemEncontrado == -1)
-			System.out.println("El número " + elem + " no se encuentra en el arreglo");
-		else
-			System.out.println("Se encontró el número " + elem + " en la posición " + posElemEncontrado + " del arreglo.");
+//		if(posElemEncontrado == -1)
+//			System.out.println("El número " + elem + " no se encuentra en el arreglo");
+//		else
+//			System.out.println("Se encontró el número " + elem + " en la posición " + posElemEncontrado + " del arreglo.");
+		
+		String[] cadenas = {"ASFFGH", "DFIGJH", "JFIREU", "AASDSS"};
+		
+		System.out.println( buscarCaracteresArray(cadenas, 0, 'A', 0) );
 	}
 	
 	/**
@@ -31,4 +36,43 @@ public class Ej_11_BuscarElementoArray {
 			return buscarElementoArray(array, ++i, elem);
 		}
 	}
+	
+	
+	public static int buscarCaracteresArray(String[] array, int i, char elem, int contChar) {
+
+		if(i != array.length) {
+						
+			int cont = buscarElementoString(array[i], 0, elem);
+			
+			if( cont == 1 ) return buscarCaracteresArray(array, ++i, elem, ++contChar);	
+			else return buscarCaracteresArray(array, ++i, elem, contChar);	
+		}
+		return contChar;
+	}
+
+	private static int buscarElementoString(String cadena, int i, char elem) {
+		
+		if(i != cadena.length()) {
+			
+			if( cadena.charAt(i) == elem ) {
+				return 1;
+			} else {
+				return buscarElementoString(cadena, ++i, elem);
+			}
+		}
+		return 0;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
